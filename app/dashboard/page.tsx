66a93780534bae6946e210b83b404c6a3dfd1948
Sachin <sachinlakshan04@gmail.com>
2024-12-10 15:11:00 +0530

    sales bar chart

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Summary from "@/app/dashboard/_components/Summary";
+import { SalesBarChart } from "@/app/dashboard/_components/SalesBarChart";
 
 const DashboardIndexPage = () => {
   return (
     <div className="p-4 grid gap-5">
       <Summary />
+
+      <div className="grid lg:grid-cols-2 gap-10">
+        <SalesBarChart />
+      </div>
     </div>
   );
 };
